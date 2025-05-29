a36b0711e3b6957fa7ef4520c2ab8fc77e7a658e
Nouraldeen504 <nouraldeen.albakoush@gmail.com>
2025-05-29 20:19:58 +0300

    deleted extra lines

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -180,9 +180,7 @@ export const orderService = {
         throw error;
       }
 
-      console.log(`order status: ${status}`);
       if(status === 'canceled'){
-        console.log("Cancelling order");
         await supabase.rpc('update_stock_after_order_cancellation', { order_id: orderId });
       }
 
